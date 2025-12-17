37bca6efeb6b80c29283279f6965c76dd8055e3d
filipe <felepesilvagalvao@gmail.com>
2025-12-17 11:15:52 -0300

    teste

--- src/components/zap/Zap.tsx
+++ src/components/zap/Zap.tsx
@@ -2,7 +2,7 @@ import styles from './Zap.module.css'
 
 function Zap() {
   return (
-    <a href="#" className={styles.zap__link}><i className="fa-brands fa-whatsapp"></i></a>
+    <a href="#" className={styles.zap__link + " zap-link"}><i className="fa-brands fa-whatsapp"></i></a>
   )
 }
 
